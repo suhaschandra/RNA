0673183df490a448622c0f8477718c9cf1bd63c0
suhaschandra <ssureshc@uci.edu>
2018-03-11 15:35:30 -0700

    Update RNASeq.R

--- RNASeq.R
+++ RNASeq.R
@@ -1,7 +1,7 @@
+#The following code has been modified from Dr Girke's systemPipeR pipeline.
+
 library(systemPipeR)
 library(GenomicFeatures)
-
-
 #Read the targets file
 targets <- read.delim("targets.txt", comment.char = "#")
 #Check if targets file is correct
@@ -9,4 +9,10 @@ targets
 #Create args
 args <- systemArgs(sysma="tophat.param", mytargets="targets.txt")
 moduleload(modules(args))
+
+resources <- list(walltime="20:00:00", ntasks=1, ncpus=cores(args), memory="1G") 
+# note this assigns 1Gb of Ram per core. If ncpus is 4 then this will amount to 4Gb total
+reg <- clusterRun(args, conffile=".BatchJobs.R", template="slurm.tmpl", Njobs=18, runid="01", resourceList=resources)
+waitForJobs(reg)
+
 
