88a48a0191c1b9fd3b84ed801ef25631e507e60b
suhaschandra <ssureshc@uci.edu>
2018-04-14 22:21:38 -0700

    Update and rename RNASeq.R to Alignment.R

--- Alignment.R
+++ Alignment.R
@@ -10,7 +10,6 @@ targets
 args <- systemArgs(sysma="tophat.param", mytargets="targets.txt")
 moduleload(modules(args))
 
-cluster.functions <- makeClusterFunctionsSLURM("slurm.tmpl")
 resources <- list(walltime="20:00:00", ntasks=1, ncpus=cores(args), memory="1G") 
 # note this assigns 1Gb of Ram per core. If ncpus is 4 then this will amount to 4Gb total
 reg <- clusterRun(args, conffile=".BatchJobs.R", template="slurm.tmpl", Njobs=18, runid="01", resourceList=resources)
