70f20c4d65f8e3a9831d44f73530d0b9b62d6397
suhaschandra <ssureshc@uci.edu>
2018-07-05 11:20:45 -0700

    Update readCounts.R
    Updated code for hierarchical clustering, sample and groupwise PCA (rlog and vsd), and 3D PCA.

--- readCounts.R
+++ readCounts.R
@@ -22,4 +22,66 @@ rpkmDFeByg <- apply(countDFeByg, 2, function(x) returnRPKM(counts=x, ranges=eByg
 write.table(countDFeByg, "results/countDFeByg.xls", col.names=NA, quote=FALSE, sep="\t")
 write.table(rpkmDFeByg, "results/rpkmDFeByg.xls", col.names=NA, quote=FALSE, sep="\t")
 
+#Hierarchical Clustering
+library(ape)
+rpkmDFeByg <- read.delim("./results/rpkmDFeByg.xls", row.names=1, check.names=FALSE)[,-19]
+rpkmDFeByg <- rpkmDFeByg[rowMeans(rpkmDFeByg) > 50,]
+d <- cor(rpkmDFeByg, method="spearman")
+hc <- hclust(as.dist(1-d))
+pdf("results/sample_tree.pdf")
+plot.phylo(as.phylo(hc), type="p", edge.col="blue", edge.width=2, show.node.label=TRUE, no.margin=TRUE)
+dev.off()
+
+#sample outlier analysis using rlog values
+library(DESeq2)
+countDF <- as.matrix(read.table("./results/countDFeByg.xls"))
+colData <- data.frame(row.names=targets$SampleName, condition=targets$Factor)
+dds <- DESeqDataSetFromMatrix(countData = countDF, colData = colData, design = ~ condition)
+d <- cor(assay(rlog(dds)), method="spearman")
+hc <- hclust(dist(1-d))
+pdf("results/sample_tree_rlog.pdf")
+plot.phylo(as.phylo(hc), type="p", edge.col=4, edge.width=3, show.node.label=TRUE, no.margin=TRUE)
+dev.off()
+                    
+#Group-wise PCA using rlog counts
+rld <- rlog(dds)
+pdf("results/PCA_group_rlog.pdf")
+plotPCA(rld)
+dev.off()
+
+#Sample-wise PCA using rlog counts
+colData <- data.frame(row.names=targets$SampleName, condition=targets$SampleName)
+dds <- DESeqDataSetFromMatrix(countData = countDF, colData = colData, design = ~ condition)
+rld <- rlog(dds)
+pdf("results/PCA_sample_rlog.pdf")
+plotPCA(rld)
+dev.off()
+                    
+#Group-wise PCA based on vsd counts
+colData <- data.frame(row.names=targets$SampleName, condition=targets$Factor)
+dds <- DESeqDataSetFromMatrix(countData = countDF, colData = colData, design = ~ condition)
+vsd <- varianceStabilizingTransformation(dds)
+pdf("results/PCA_group_vsd.pdf")
+plotPCA(vsd)
+dev.off()
+
+#Sample-wise PCA based on vsd counts
+colData <- data.frame(row.names=targets$SampleName, condition=targets$SampleName)
+dds <- DESeqDataSetFromMatrix(countData = countDF, colData = colData, design = ~ condition)
+vsd <- varianceStabilizingTransformation(dds)
+pdf("results/PCA_sample_vsd.pdf")
+plotPCA(vsd)
+dev.off()
+
+#Groupwise 3D PCA using rlog
+library(scatterplot3d)
+colData <- data.frame(row.names=targets$SampleName, condition=targets$Factor)
+rld <- rlog(dds)
+pca <- prcomp(t(assay(rld)), center=TRUE)
+pca2 <- pca$x
+groups <- targets$Factor
+pdf("results/3D_leans.pdf")
+scatterplot3d(pca2[,1], pca2[,2], pca2[,3], color = as.numeric(groups), pch=19, type='h')
+dev.off()
+
 
